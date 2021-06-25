599c8c3f096d84f1417a6e871c1d07e679f83ca6
Tam Le <philosocode@gmail.com>
2021-06-25 11:43:03 -0600

    Display loaded restaurants

--- src/components/__tests__/RestaurantList.spec.js
+++ src/components/__tests__/RestaurantList.spec.js
@@ -7,8 +7,28 @@ describe("RestaurantList", () => {
     // can check if it was called
     const loadRestaurants = jest.fn().mockName("loadRestaurants");
 
-    render(<RestaurantList loadRestaurants={loadRestaurants} />);
+    render(
+      <RestaurantList loadRestaurants={loadRestaurants} restaurants={[]} />,
+    );
 
     expect(loadRestaurants).toHaveBeenCalled();
+  });
+
+  it("displays the restaurants", () => {
+    // func that does nothing
+    const noop = () => {};
+
+    const restaurants = [
+      {id: 1, name: "Sushi Place"},
+      {id: 2, name: "Pizza Place"},
+    ];
+
+    const {queryByText} = render(
+      <RestaurantList loadRestaurants={noop} restaurants={restaurants} />,
+    );
+
+    // queryByText: find an EL containing the text
+    expect(queryByText("Sushi Place")).not.toBeNull();
+    expect(queryByText("Pizza Place")).not.toBeNull();
   });
 });
