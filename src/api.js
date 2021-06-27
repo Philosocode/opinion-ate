9135f5239534d9008f76a3ed453efd80be34ff1b
Tam Le <philosocode@gmail.com>
2021-06-27 15:30:19 -0600

    Add test for loading and storing restaurants from API

---
+++ src/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const client = axios.create({
+  baseURL: "https://outside-in-dev-api.herokuapp.com/O5qZzrSNoJwXvPHIQ5geBAkhAYxyZtAK"
+});
+
+const api = {
+  loadRestaurants() {
+    return client.get("/restaurants").then(response => response.data);
+  },
+};
+
+export default api;
