f1a48c7aca28822973e5cc96259144e828ba93e7
Tam Le <philosocode@gmail.com>
2021-06-30 10:30:05 -0600

    Add tests for loading indicator

--- src/store/__tests__/restaurants.spec.js
+++ src/store/__tests__/restaurants.spec.js
@@ -4,13 +4,47 @@ import restaurantsReducer from "../restaurants/reducers";
 import {loadRestaurants} from "../restaurants/actions";
 
 describe("restaurants", () => {
-  describe("loadRestaurants action", () => {
-    it("stores the restaurants", async () => {
-      const records = [
-        {id: 1, name: "Sushi Place"},
-        {id: 2, name: "Pizza Place"},
-      ];
+  describe("initially", () => {
+    const initialState = {};
+    const store = createStore(
+      restaurantsReducer,
+      initialState,
+      applyMiddleware(thunk),
+    );
 
+    expect(store.getState().loading).toEqual(false);
+  });
+
+  describe("while loading", () => {
+    it("sets a loading flag", () => {
+      const api = {
+        // promise that never resolves
+        loadRestaurants: () => new Promise(() => {}),
+      };
+
+      const initialState = {};
+
+      const store = createStore(
+        restaurantsReducer,
+        initialState,
+        applyMiddleware(thunk.withExtraArgument(api)),
+      );
+
+      store.dispatch(loadRestaurants());
+
+      expect(store.getState().loading).toEqual(true);
+    });
+  });
+
+  describe("when loading succeeds", () => {
+    const records = [
+      {id: 1, name: "Sushi Place"},
+      {id: 2, name: "Pizza Place"},
+    ];
+
+    let store;
+
+    beforeEach(() => {
       // stub API
       const api = {
         loadRestaurants: () => Promise.resolve(records),
@@ -22,16 +56,23 @@ describe("restaurants", () => {
 
       // create the store
       // only the needed reducer is passed in
-      const store = createStore(
+      store = createStore(
         restaurantsReducer,
         initialState,
         // inject API into thunk; make available to all thunk funcs
         applyMiddleware(thunk.withExtraArgument(api)),
       );
 
-      await store.dispatch(loadRestaurants());
+      // return the Promise to make Jest wait for it
+      return store.dispatch(loadRestaurants());
+    });
 
+    it("stores the restaurants", () => {
       expect(store.getState().records).toEqual(records);
+    });
+
+    it("clears the loading flag", () => {
+      expect(store.getState().loading).toEqual(false);
     });
   });
 });
--- src/store/restaurants/actions.js
+++ src/store/restaurants/actions.js
@@ -1,12 +1,17 @@
+export const START_LOADING = "START_LOADING";
 export const STORE_RESTAURANTS = "STORE_RESTAURANTS";
 
 export const loadRestaurants = () => (dispatch, getState, api) => {
+  dispatch(startLoading());
   api.loadRestaurants().then(records => {
     dispatch(storeRestaurants(records));
   });
 };
 
-const storeRestaurants = records => ({
-  type: STORE_RESTAURANTS,
-  records,
-});
+function startLoading() {
+  return {type: START_LOADING};
+}
+
+function storeRestaurants(records) {
+  return {type: STORE_RESTAURANTS, records};
+}
